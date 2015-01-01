c48864202cc199bcb27b8cd1d0c9019dabc2c40b
oksayt <oxxide@gmail.com>
2015-01-02 03:45:54 +0900

    Somewhat working CLI arg parsing, preliminary html parsing

--- src/DataStore.scala
+++ src/DataStore.scala
@@ -1,22 +1,65 @@
-import java.io.FileOutputStream
+import java.io.{FileInputStream, FileOutputStream}
+import java.net.URL
+import java.nio.charset.Charset
+import java.nio.file.{Paths, Files}
+import org.apache.commons.io.IOUtils
+import java.io.StringReader
 import java.net.URL
 import java.nio.charset.Charset
 
+import nu.validator.htmlparser.common.XmlViolationPolicy
+import nu.validator.htmlparser.sax.HtmlParser
 import org.apache.commons.io.IOUtils
+import org.xml.sax.InputSource
 
+import scala.xml.Node
+import scala.xml.parsing.NoBindingFactoryAdapter
 /**
  * TODO: Describe type
  */
+class Concert(val title: String, val date: String, val time: String, val
+program: String) {
+  override def toString: String = {
+    "%s\n%s, %s\n%s".format(title, date, time, program)
+  }
+}
+
 class DataStore {
   val root = "C:\\cygwin\\home\\ozan\\scalper-data\\html"
+  val baseUrl = "http://www.ongakunotomo.co.jp/concert/detail.php?id="
 
   def writeHtml(url: String, id: String): Unit = {
-    IOUtils
-      .write(IOUtils.toByteArray(new URL(url).openStream()),
-        new FileOutputStream(localHtmlPath(id)))
+    println("Fetching url " + url)
+    IOUtils.write(IOUtils.toByteArray(new URL(url).openStream()), new
+        FileOutputStream(localHtmlPath(id)))
   }
 
   def localHtmlPath(id: String): String = {
     root + "\\" + id + ".html"
+  }
+
+  def toNode(str: String): Node = {
+    val parser = new HtmlParser
+    parser.setNamePolicy(XmlViolationPolicy.ALLOW)
+
+    val saxer = new NoBindingFactoryAdapter
+    parser.setContentHandler(saxer)
+    parser.parse(new InputSource(new StringReader(str)))
+
+    saxer.rootElem
+  }
+
+  def lookup(id: String): Concert = {
+    if (!Files.exists(Paths.get(localHtmlPath(id)))) {
+      writeHtml(baseUrl + id, id)
+    }
+    val bodyString = IOUtils.toString(new FileInputStream(localHtmlPath(id)),
+      Charset.forName("EUC-JP"))
+    val node = toNode(bodyString)
+    val title = ((node \\ "table" \ "tbody" \ "tr")(1) \\ "h1").text
+    val date = (node \\ "table" \ "tbody" \\ "p" \ "span").text
+    val time = (node \\ "table" \ "tbody" \\ "p").text
+    val program = (((node \\ "table" \ "tbody" \\ "tr")(3) \ "td")(3) \ "h2").text
+    new Concert(title, date, time, program)
   }
 }
--- src/Scalper.scala
+++ src/Scalper.scala
@@ -1,10 +1,7 @@
 import java.io.StringReader
-import java.net.URL
-import java.nio.charset.Charset
 
 import nu.validator.htmlparser.common.XmlViolationPolicy
 import nu.validator.htmlparser.sax.HtmlParser
-import org.apache.commons.io.IOUtils
 import org.xml.sax.InputSource
 
 import scala.xml.Node
@@ -12,24 +9,59 @@ import scala.xml.parsing.NoBindingFactoryAdapter
 
 object Scalper {
   def main(args: Array[String]) {
-    val baseUrl = "http://www.ongakunotomo.co.jp/concert/detail.php?id="
     val dataStore = new DataStore
-    (80100 until 80200).map((x) => {
-      dataStore.writeHtml(baseUrl + x.toString,
-        x.toString)
-    })
 
-    /*
-    val bodyString = IOUtils
-      .toString(new URL(testUrl).openStream(), Charset.forName("EUC-JP"))
+    sealed trait Command {def name: String}
+    case object LOOKUP extends Command {val name = "lookup"}
 
-    val node = toNode(bodyString)
-    println("Blah")
-    val mojibake = (node \\ "table" \ "tbody" \\ "p" \ "span").text
-    println(mojibake)
-    */
+    val commands = Map() ++ (Vector(LOOKUP) map (t => t.name -> t))
+
+    type OptionMap = Map[Symbol, Any]
+    def parseOptions(list: List[String], parsed: Map[Symbol, Any]):
+    Map[Symbol, Any] = {
+      list match {
+      case Nil => parsed
+      case "--command" :: value :: tail =>
+        parseOptions(tail, parsed ++ Map('command -> commands.get(value)))
+      case "--id" :: value :: tail =>
+        parseOptions(tail, parsed ++ Map('id -> value))
+      case "--delay_ms" :: value :: tail =>
+        parseOptions(tail, parsed ++ Map('delay_ms -> value.toInt))
+      case option :: tail => throw new IllegalArgumentException("Unknown " +
+        "option: " + option)
+      }
+    }
+    val options = parseOptions(args.toList, Map('delay_ms -> 500))
+    println(options)
+    if (!(options contains 'command)) {
+      throw new IllegalArgumentException("Missing or unknown --command, known" +
+        " commands are: " + (commands.keySet mkString ", "))
+    }
+
+    options.get('command).get match {
+    case Some(LOOKUP) => println(dataStore.lookup(options.get('id).get
+      .asInstanceOf[String]))
+    }
+
   }
 
+  /*
+  val baseUrl = "http://www.ongakunotomo.co.jp/concert/detail.php?id="
+  val dataStore = new DataStore
+  (80100 until 80200).map((x) => {
+    dataStore.writeHtml(baseUrl + x.toString,
+      x.toString)
+  })
+  */
+  /*
+  val bodyString = IOUtils
+    .toString(new URL(testUrl).openStream(), Charset.forName("EUC-JP"))
+
+  val node = toNode(bodyString)
+  println("Blah")
+  val mojibake = (node \\ "table" \ "tbody" \\ "p" \ "span").text
+  println(mojibake)
+  */
   def toNode(str: String): Node = {
     val parser = new HtmlParser
     parser.setNamePolicy(XmlViolationPolicy.ALLOW)
